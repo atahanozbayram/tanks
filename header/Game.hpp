85c487dc3bfda9e6f23619fd2b2b5f91859fc2a8
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-02-19 21:48:17 +0300

    Made little change to cmake file and so on
    I did changed GLOB feature to GLOB_RECURSE in the cmake file for the
    source code files. Later on I have deleted all the methods and members.
    I did this because I think there is major design problem with this
    class.

--- header/Game.hpp
+++ header/Game.hpp
@@ -23,27 +23,6 @@ public:
 
 private:
 // Private utility functions
-	void menuLoop(Window* windowPtr, Event* eventPtr);
-	void gameLoop(Window* windowPtr, Event* eventPtr);
-	void handleInput();
-	void updateGame(Window *windowPtr, Event *eventPtr);
-	void renderWindow(Window* windowPtr);
-	void updateSfmlEvents(Window* windowPtr, Event* eventPtr);
-
-	Window* initWindow();
-	Event* initEvent();
-	State* initState();
-
-	
-	std::function<void()> initGame();
-	
-
-	// private members
-	Window* m_window;
-	Event* m_event;
-	State* m_state;
-	SettingManager m_settingManager;
-	EntityManager m_entityManager;
 };
 
 
---
+++ header/Manager.hpp
@@ -0,0 +1,17 @@
+#ifndef ATA_MANAGER_HPP
+#define ATA_MANAGER_HPP
+
+namespace ata
+{
+class Manager
+{
+public:
+	Manager();
+	~Manager();
+
+
+private:
+};
+}
+
+#endif
