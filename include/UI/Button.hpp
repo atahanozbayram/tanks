f9c169df83089384712464565e35bb93fe2ae728
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-03-02 14:19:29 +0300

    Added new folder for UI system

---
+++ include/UI/Button.hpp
@@ -0,0 +1,16 @@
+#ifndef ATA_BUTTON_HPP
+#define ATA_BUTTON_HPP
+
+#include "UI/Widget.hpp"
+
+namespace ata
+{
+class Button : public ata::Widget
+{
+public:
+	
+
+};
+}
+
+#endif
---
+++ include/UI/Widget.hpp
@@ -0,0 +1,13 @@
+#ifndef ATA_WIDGET_HPP
+#define ATA_WIDGET_HPP
+
+namespace ata
+{
+class Widget
+{
+public:
+
+};
+}
+
+#endif
